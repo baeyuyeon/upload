c6d285acce6d42a0094380aefd5d62f32a890734
baeyuyeon <dbdus983@naver.com>
2023-06-01 13:32:31 +0900

    파일업로드

---
+++ src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,59 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFilePath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFiles.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFilePath(storeFileName)));
+
+        return new UploadFile(originalFilename, storeFileName);
+
+    }
+
+    private String createStoreFileName(String originalFilename) {
+
+        String uuid = UUID.randomUUID().toString();
+        String ext = extracted(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private String extracted(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
